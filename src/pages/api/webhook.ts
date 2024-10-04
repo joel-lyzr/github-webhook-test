93b57e703d0d28236c56e38a9c8189b1041ae73f
joel-lyzr <joel@lyzr.ai>
2024-10-04 11:21:40 +0530

    ✨feature: add amplify webhook

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -5,9 +5,20 @@ type Data = {
   data: object;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ data: req.body });
+  const response = await fetch(
+    "https://webhooks.amplify.us-east-1.amazonaws.com/prod/webhooks?id=9b311be4-47eb-4923-8bc2-08f74b4aa497&token=c0uUSJgTcrJnGPpXmkmkYkzoDzyzHY3N0eQCbcNrY&operation=startbuild",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    }
+  );
+  const data = await response.json();
+  res.status(200).json({ data });
 }
